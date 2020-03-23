7bfda6dd65c0c4a32756c44b6a8260ac7e8f7c74
IMM9O <islam.muhamed.90+github@gmail.com>
2020-03-23 20:35:36 +0200

    Fisr commit

---
+++ data.js
@@ -0,0 +1,49 @@
+const main = {
+  name: 'Islam Muhammad',
+  mail: 'islam.muhamed.90@gmail.com',
+  img:
+    'https://media-exp1.licdn.com/dms/image/C4D03AQEohXhQ_3N44g/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=fF0SYVLUvSwux-lG5tVTDpAUoari2e4dShPffr6XX5w',
+  role: ['FrontEnd Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://sivanesh-s.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/IMM9O'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/islam-muhammad/'
+    },
+    // {
+    //   name: 'StackOverflow',
+    //   iconName: 'logo-stackoverflow',
+    //   link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
+    // },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/IMM9OO'
+    },
+    // {
+    //   name: 'npm',
+    //   iconName: 'logo-npm',
+    //   link: 'https://www.npmjs.com/~sivanesh_s'
+    // },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'islam.muhamed.90@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: 'https://github.com/IMM9O?tab=repositories' },
+    { name: 'Blogs', link: 'https://dev.to/imm9o' }
+  ]
+};
