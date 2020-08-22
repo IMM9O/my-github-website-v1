4c54f417177c5c5ded002a1b485c6a2b59479601
IMM9O <islam.muhamed.90+github@gmail.com>
2020-08-22 21:06:13 +0200

    Update fetch data from dev function

--- src/_data/dev.js
+++ src/_data/dev.js
@@ -5,24 +5,38 @@ var url =
 var postUrl = 'https://dev.to/api/articles';
 var authorLink = 'https://dev.to/imm9o';
 
+// Get One Post
 const getPostData = async (item) => {
   return axios.get(`${postUrl}/${item.id}`);
 };
 
+// Get Array of promises to get each post details 
 const getPostsData = async (list) => {
   return Promise.all(list.map((item) => getPostData(item)));
 };
 
-module.exports = () => {
-  return new Promise((resolve, reject) => {
-    axios.get(url).then((response) => {
-      const blogList = [...response.data];
-      getPostsData(blogList).then((res) => {
-        const posts = res.map((responses) => {
-          return { ...responses.data, authorLink };
-        });
-        resolve({ url, posts });
-      });
-    });
-  });
+const getData = async () => {
+  const { data } = await axios.get(url);
+  const posts = await getPostsData(data);
+  return posts.map(post => ({ ...post.data }));
 };
+
+module.exports = getData;
+
+// module.exports = () => {
+//   return new Promise((resolve, reject) => {
+//     axios.get(url).then((response) => {
+//       const blogList = [...response.data];
+//       getPostsData(blogList).then((res) => {
+//         const posts = res.map((responses) => {
+//           return { ...responses.data, authorLink };
+//         });
+//         resolve(posts);
+//       });
+//     });
+//   });
+// };
+
+// getData().then(res => {
+//   console.log(res)
+// });
